1694b2802a50b450110f4291d7ace4436413fe8b
xaazias <flamesuspect@gmail.com>
2021-11-03 22:55:59 +0300

    Initial commit.

---
+++ solution/index.js
@@ -0,0 +1,83 @@
+module.exports = class {
+  constructor(array) {
+    const types = { boolean: {}, number: {}, string: {} },
+      res = []
+
+    const data = array.filter((item) => {
+      const type = typeof item
+
+      if (type in types)
+        return types[type].hasOwnProperty(item)
+          ? false
+          : (types[type][item] = true)
+      else return res.indexOf(item) >= 0 ? false : res.push(item)
+    })
+
+    this.data = data
+    this.size = data.length
+  }
+
+  [Symbol.iterator]() {
+    let index = -1
+    const data = this.data
+
+    return {
+      next: () => ({ value: data[++index], done: !(index in data) })
+    }
+  }
+
+  get [Symbol.toStringTag]() {
+    return "^_^"
+  }
+
+  toString() {
+    return "[object ^_^]"
+  }
+
+  keys() {
+    const temp = []
+    for (let i = 0; i < this.data.length; i++) temp.push(this.data[i])
+
+    return temp
+  }
+
+  values() {
+    const temp = []
+    for (let i = 0; i < this.data.length; i++) temp.push(this.data[i])
+
+    return temp
+  }
+
+  entries() {
+    const temp = []
+    for (let i = 0; i < this.data.length; i++)
+      temp.push([this.data[i], this.data[i]])
+
+    return temp
+  }
+
+  clear() {
+    this.data = []
+    this.size = 0
+  }
+
+  add(item) {
+    if (this.data.indexOf(item) === -1) this.data.push(item)
+    return this
+  }
+
+  delete(item) {
+    const index = this.data.indexOf(item)
+    if (index !== -1) this.data.splice(index, 1)
+  }
+
+  has(item) {
+    return this.data.indexOf(item) === -1 ? false : true
+  }
+
+  forEach(callback, context) {
+    for (let i = 0; i < this.data.length; i++) {
+      callback.call(context, this.data[i], i, this.data)
+    }
+  }
+}
